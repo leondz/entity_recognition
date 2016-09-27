468368fda78890813a53483fe12a7c565bc23f29
Leon Derczynski <leonderczynski@gmail.com>
2016-09-27 15:27:01 +0200

    make feature names crfsuite friendly, tabs

--- base_extractors.py
+++ base_extractors.py
@@ -17,40 +17,43 @@ def word2features(sent, i):
     if i > 0:
         word1 = sent[i-1]
         features.extend([
-            '-1:word.lower=' + word1.lower(),
-            '-1:word.istitle=%s' % word1.istitle(),
-            '-1:word.isupper=%s' % word1.isupper(),
+            '-1word.lower=' + word1.lower(),
+            '-1word.istitle=%s' % word1.istitle(),
+            '-1word.isupper=%s' % word1.isupper(),
         ])
     else:
         features.append('BOS')
+
         
     if i < len(sent)-1:
         word1 = sent[i+1]
         features.extend([
-            '+1:word.lower=' + word1.lower(),
-            '+1:word.istitle=%s' % word1.istitle(),
-            '+1:word.isupper=%s' % word1.isupper(),
+            '+1word.lower=' + word1.lower(),
+            '+1word.istitle=%s' % word1.istitle(),
+            '+1word.isupper=%s' % word1.isupper(),
         ])
     else:
         features.append('EOS')
-                
+
     return features
 
 # takes a list of token/label pairs; returns a list of [feature]/label pairs
 def featurise(sentence, brown_cluster = {}):
 
-	sentence_repr = []
+    sentence_repr = []
 
-	for i in range(len(sentence)):
-		features = []
+    for i in range(len(sentence)):
+        features = []
 
-		word = sentence[i]
-		if word in brown_cluster:
-			for j in range(1,len(brown_cluster[word])+1):
-				features.append('p' + str(j) + 'b' + brown_cluster[word][0:j])
+        # add brown features
+        word = sentence[i]
+        if word in brown_cluster:
+            for j in range(1,len(brown_cluster[word])+1):
+                features.append('p' + str(j) + 'b' + brown_cluster[word][0:j])
 
-		features += word2features(sentence, i)
+        # add per-word features
+        features += word2features(sentence, i)
 
-		sentence_repr.append(features)
+        sentence_repr.append(features)
 
-	return(sentence_repr)
+    return(sentence_repr)
