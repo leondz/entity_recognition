6ad7de13b2088eac6af7fc99dcffd87a2ba64cab
Leon Derczynski <leonderczynski@gmail.com>
2016-09-27 16:36:01 +0200

    link with setuptools

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+	author='Leon Derczynski, Isabelle Augenstein',
+	author_email='leonderczynski@gmail.com',
+	include_package_data=True,
+	install_requires=['python-crfsuite', 'nltk', 'sklearn', 'scipy', 'numpy', 'json'],
+	license='Apache 2.0',
+	long_description=open('README.md').read(),
+	name='entity_recognition',
+	scripts=['er'],
+	summary='Toolkit for recognising named entities through structured labeling',
+	url='https://github.com/leondz/entity_recognition/',
+	version='1.0',
+	entry_points={'console_scripts':['run_tagger=run_tagger.py:main', 'train_tagger=train_tagger.py:main']},
+	)
